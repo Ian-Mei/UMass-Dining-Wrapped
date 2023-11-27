fd6439607387d85462cb42634397154d8d053fe2
DatDragon100 <39425237+DatDragon100@users.noreply.github.com>
2023-11-27 01:30:18 -0500

    idk

--- testpdf.py
+++ testpdf.py
@@ -5,37 +5,36 @@ import requests
 import tabula
 from datetime import date
 
-with open('cookies.json', 'r') as file:
+with open(r"UMass-Dining-Wrapped\cookies.json", "r") as file:
     cookies_list = json.load(file)
 
 # Convert the list of cookies to a CookieJar
-cookies = requests.utils.cookiejar_from_dict({cookie['name']: cookie['value'] for cookie in cookies_list})
+cookies = requests.utils.cookiejar_from_dict(
+    {cookie["name"]: cookie["value"] for cookie in cookies_list}
+)
 
 # Create a session and set the loaded cookies
 session = requests.Session()
 session.cookies = cookies
 
+
 def download_pdf(url, save_path):
     response = session.get(url)
-    with open(save_path, 'wb') as pdf_file:
+    with open(save_path, "wb") as pdf_file:
         pdf_file.write(response.content)
 
-def addpage(url,save_path):
-    pdf = open(save_path,'rb')
-    
-    
 
-if __name__ == '__main__':
 
+
+
+if __name__ == "__main__":
     today = date.today()
     year = today.year
     month = today.month
     day = today.day
-    while(month>today.month-5):
-        path = f"https://get.cbord.com/umass/full/historyPDF.php?dateS={year}-{month}-{day-14}&dateE={year}-{month}-{day}"
-        save_path = "info.pdf"
-        download_pdf(path,save_path)
+    path = f"https://get.cbord.com/umass/full/historyPDF.php?dateS={year}-{month}-{day-14}&dateE={year}-{month}-{day}"
+    save_path = "UMass-Dining-Wrapped\info.pdf"
+    download_pdf(path, save_path)
 
-    tables = tabula.read_pdf("info.pdf", pages="all")
+    tables = tabula.read_pdf("UMass-Dining-Wrapped\info.pdf", pages="all")
     print(tables)
-    
